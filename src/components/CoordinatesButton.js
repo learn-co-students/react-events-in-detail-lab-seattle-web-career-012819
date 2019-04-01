6d2e56518537b7484274c050dff0e6854095153a
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-04-01 16:57:00 -0700

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,14 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+
+  onClickButton(event){
+    let array = []
+    array.push(event.clientX)
+    array.push(event.clientY)
+    this.props.onReceiveCoordinates(array)}
+
+  render(){
+    return <button onClick={(event) => this.onClickButton(event)}></button>
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,13 @@
-// Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component{
+
+  onClickButton(event) {
+    event.persist()
+    setTimeout(() => {this.props.onDelayedClick(event)}, this.props.delay)
+  }
+
+  render(){
+    return <button onClick={(event) => this.onClickButton(event)}></button>
+  }
+}
