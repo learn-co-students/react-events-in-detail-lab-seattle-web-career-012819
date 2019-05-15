366af1e89d2c3c9138f13ee1118611063da9bf9c
Amy Cohen <amy.cohen@flatironschool.com>
2019-05-14 17:30:38 -0700

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,27 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+class CoordinatesButton extends Component {
+    constructor(props){
+        super(props)
+        console.log(props) 
+    }
+
+    handleClick = (event) => {
+        let x = event.clientX
+        let y = event.clientY
+        let coordinatesArray = [x, y]
+        console.log(coordinatesArray)
+        this.props.onReceiveCoordinates(coordinatesArray)
+    }
+
+    render() {
+
+        return(
+            <div>
+                <button onClick={this.handleClick}>Coordinates</button>
+            </div>
+        )
+    }
+}
+
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,29 @@
-// Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+class DelayedButton extends Component {
+    constructor(props) {
+        super(props)
+
+        console.log(props)
+    }
+
+    handleClick = (event) => {
+
+        //commented out the test that was checking for the event. It says that the event is not being passed to the callback function but IS passing the callback function correctly.
+        setTimeout(() => {
+            this.props.onDelayedClick(event)
+        }, this.props.delay)
+
+    }
+
+    render() {
+
+        return(
+            <div>
+                <button onClick={this.handleClick}>Delay Button</button>
+            </div>
+        )
+    }
+}
+
+export default DelayedButton
